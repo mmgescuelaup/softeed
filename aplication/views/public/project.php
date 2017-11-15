780c7c6705e2296dc7458b884f4e7950297390b3
= <=>
2017-11-14 21:25:52 -0500

    Profiles - Projects
    Se añadieron los diseños públicos del perfil y de los projectos.

--- aplication/views/public/project.php
+++ aplication/views/public/project.php
@@ -20,7 +20,46 @@
             </div>
         </div>
         <div class="right">
-              
+            <div class="developer">
+                <div class="image">                    
+                    <img src="aplication/images/sample-profile.jpg" alt="">
+                </div>
+                <div class="title">
+                    <h3><a href="#">Thomas Cruise Maphoner</a></h3>
+                    <h4>@ThomCruze</h4>
+                </div>                  
+            </div>
+            <div class="sales full">                
+                <div class="price">
+                    <h2>$50 MXN</h2>
+                </div>
+                <div class="buy">
+                    <a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i>Buy Now</a>
+                </div>
+                <div class="pay-methods full">
+                    <img src="aplication/images/paypal.png" alt="">
+                    <img src="aplication/images/visa.png" alt="">
+                    <img src="aplication/images/mastercard.png" alt="">
+                    <img src="aplication/images/americanexpress.png" alt="">
+                </div>
+            </div>
+            <div class="tags full">                
+                <div class="tag">
+                    <a href="#">Android</a>
+                </div>
+                <div class="tag">
+                    <a href="#">App</a>
+                </div>
+                <div class="tag">
+                    <a href="#">Software</a>
+                </div>
+                <div class="tag">
+                    <a href="#">Android</a>
+                </div>
+                <div class="tag">
+                    <a href="#">Medical</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
