9054c19f2a14912f7eb570ade6b0a1ff8ac2e300
mtmg <mmgescuelaup@users.noreply.github.com>
2017-11-21 00:42:24 -0500

    Eliminia la altura

--- signup.php
+++ signup.php
@@ -23,7 +23,6 @@
             background-color: #F0F4F8;
             background: #252525 url(aplication/images/nodeatles.png) no-repeat fixed center;
             width: 100%;
-            height: 100%;
             display: flex;
             flex-direction: column;
             align-items: center;
