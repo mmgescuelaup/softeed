620b32753b61a7eb85f333f1bff0cf922896b6ed
Sergio A. Trejo <alejandrotrejocode@gmail.com>
2017-11-30 07:03:25 -0500

    Changes para la exposición

--- aplication/views/public/showcase.php
+++ aplication/views/public/showcase.php
@@ -13,29 +13,29 @@
             <div class="categories">
                 <div class="category">
                     <img src="aplication/images/icons/software.png" alt="">
-                    <a href="#">Software</a>
+                    <a href="<?php echo $helper->url('software','index'); ?>">Software</a>
                 </div>
                 <div class="category">
                     <img src="aplication/images/icons/diseno.png" alt="">
-                    <a href="#">Diseño</a>
+                    <a href="<?php echo $helper->url('diseno','index'); ?>">Diseño</a>
                 </div>
                 <div class="category">
                     <img src="aplication/images/icons/ecologia.png" alt="">
-                    <a href="#">Ecología</a>
+                    <a href="<?php echo $helper->url('ecologia','index'); ?>">Ecología</a>
                 </div>
             </div>
             <div class="categories">
                 <div class="category">
                     <img src="aplication/images/icons/turismo.png" alt="">
-                    <a href="#">Turismo</a>
-                </div>
+                    <a href="<?php echo $helper->url('turismo','index'); ?>">Turismo</a>
+                </div>            
                 <div class="category">
                     <img src="aplication/images/icons/fotografia.png" alt="">
-                    <a href="#">Fotografía</a>
+                    <a href="<?php echo $helper->url('fotografia','index'); ?>">Fotografía</a>
                 </div>
                 <div class="category">
                     <img src="aplication/images/icons/administracion.png" alt="">
-                    <a href="#">Administración</a>
+                    <a href="<?php echo $helper->url('administracion','index'); ?>">Administración</a>
                 </div>
             </div>
         </div>
